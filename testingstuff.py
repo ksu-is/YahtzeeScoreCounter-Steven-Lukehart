c751e71cfaffc71889a816ab823e715410b689ab
Treb-s <slukehar@students.kennesaw.edu>
2021-11-20 15:46:07 -0500

    Update testingstuff.py

--- testingstuff.py
+++ testingstuff.py
@@ -1,25 +1,25 @@
 def singleGame():
     print(format("Yahtzee Score Card"))
     print(format("Upper Section"
-    print(format(aces))
-    print(format(twos))
-    print(format(threes))
-    print(format(fours))
-    print(format(fives))
-    print(format(sixes))
+    print(format("1"),(aces))
+    print(format("2"),(twos))
+    print(format("3"),(threes))
+    print(format("4"),(fours))
+    print(format("5"),(fives))
+    print(format("6"),(sixes))
     print(format(upperTotal))
     print(format(upperBonus))
     print("")
     print(format("Lower Section"))
-    print(format(threeOfKind))
-    print(format(fourOfKind))
-    print(format(smallStraight))
-    print(format(largeStraight))
-    print(format(yahtzee))
-    print(format(chance))
+    print(format("7"),(threeOfKind))
+    print(format("8"),(fourOfKind))
+    print(format("9"),(smallStraight))
+    print(format("10"),(largeStraight))
+    print(format("11"),(yahtzee))
+    print(format("12"),(chance))
     print(format(lowerTotal))
 
-    holdPrompt = input("Swap any dice? (y for HITME, n for HOLD): ")
+    print("Press the number Correlated to the box you'd like to add a score to.")
     print("")
     holdPrompt.replace(" ", "")
     holdPrompt.lower()
@@ -29,6 +29,12 @@ def singleGame():
         print("")
         holdPrompt.replace(" ", "")
         holdPrompt.lower()
+
+def upperTotal():
+    upperTotal = aces + twos + threes + fours + fives + sixes
+
+def upperBonus():
+    upperTotal > 63
 
 def main():
     aces = 0
@@ -60,26 +66,5 @@ def main():
     print(name)
     print(format("Game 1", '>43s'))
     print("")
-    game = list(game)
-    for x in game:
-        if x == True:
-            statIndex = game.index(x)
-    if statIndex == 0:
-        yahtzee += 1
-    elif statIndex == 1:
-        fullHouse += 1
-    elif statIndex == 2:
-        smallStraight += 1
-    elif statIndex == 3:
-        largeStraight += 1
-    elif statIndex == 4:
-        fourOfKind += 1
-    elif statIndex == 5:
-        threeOfKind += 1
-    else:
-        pass
-    print("")
-    gameCount = 1
-    print("END OF GAME", gameCount)
-    print("-" * 80)
-    print("")
+    input("Press Enter to continue")
+    game = singleGame()
